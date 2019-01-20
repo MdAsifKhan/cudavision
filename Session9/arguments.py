7e00a621eb1db428b5422e7157071fb761ff34f8
MdAsifKhan <mak4086@gmail.com>
2019-01-20 19:07:27 +0100

    update net2 and net3

--- Session9/arguments.py
+++ Session9/arguments.py
@@ -6,25 +6,24 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', required=False, help='soccer| sequence', default='soccer')
 parser.add_argument('--data_root', required=False, help='path to dataset', default='')
 parser.add_argument('--workers', type=int, help='number of data loading workers', default=2)
-parser.add_argument('--batch_size', type=int, default=32, help='input batch size')
+parser.add_argument('--batch_size', type=int, default=8, help='input batch size')
 
-parser.add_argument('--nm_epochs', type=int, default=25, help='number of epochs for training')
-parser.add_argument('--l2 regularization', type=float, default=1e-5, help='l2 parameter')
+parser.add_argument('--nm_epochs', type=int, default=50, help='number of epochs for training')
+parser.add_argument('--l2 regularization', type=float, default=0.0, help='l2 parameter')
 parser.add_argument('--optimizer', type=str, default='adam', help='optimizer to use')
-parser.add_argument('--print_every', type=int, default=5, help='print checkpoints')
+parser.add_argument('--print_every', type=int, default=2, help='print checkpoints')
 parser.add_argument('--save_every', type=int, default=5, help='model checkpoints')
 
 parser.add_argument('--resume', type=int, default=0, help='epoch at which training resumes')
 parser.add_argument('--model_root', default='', help='folder to output model checkpoints')
 parser.add_argument('--result_root', default='', help='folder to output image checkpoints')
 parser.add_argument('--use_gpu', type=bool, default=True, help='Enable GPU Training')
-parser.add_argument('--manualSeed', type=int, help='manual seed')
+parser.add_argument('--manualSeed', type=int, default=123, help='manual seed')
 parser.add_argument('--image', required=False, help='test image path', default='')
 parser.add_argument('--xml', required=False, help='test xml path', default='')
 
 opt = parser.parse_args()
 opt.dataset = 'soccer'
-opt.manualSeed = 123
 opt.input_size = (640, 480)
 opt.rot_degree = 45
 if opt.data_root == '':
@@ -55,9 +54,3 @@ elif opt.optimizer=='rmsprop':
 	opt.alpha = 0.995
 else:
 	ValueError('Optimizer Not Supported')
-
-opt.l2 = 0.0
-opt.nm_epochs = 50
-opt.save_every = 5
-opt.batch_size = 8
-opt.print_every = 2
--- Session9/model.py
+++ Session9/model.py
@@ -146,11 +146,11 @@ class SweatyNet2(nn.Module):
         super(SweatyNet2,self).__init__()
         self.nc = nc
         self.layer1 = nn.Sequential(
-                        nn.Conv2d(3, 8, 3, padding=0),
+                        nn.Conv2d(3, 8, 3, padding=1),
                         nn.BatchNorm2d(8),
                         nn.ReLU()
         )
-        self.pool = nn.nn.MaxPool2d(2)
+        self.pool = nn.nn.MaxPool2d(2, stride=2)
         
         self.layer2 = nn.Sequential(
                         nn.Conv2d(8, 16, 3, padding=1),
@@ -195,10 +195,9 @@ class SweatyNet2(nn.Module):
                         nn.ReLU()
 
         )
-        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear')
         
         self.layer9 = nn.Sequential(
-                        nn.Conv2d(8+16+32+64+64, 64, 3, padding=1),
+                        nn.Conv2d(8+16+32+64+64, 64, 3, padding=0),
                         nn.BatchNorm2d(64),
                         nn.ReLU()
 
@@ -222,15 +221,13 @@ class SweatyNet2(nn.Module):
 
         )       
         self.layer13 = nn.Sequential(
-                        nn.Conv2d(16, 16, 3, padding=0),
+                        nn.Conv2d(16, 16, 3, padding=1),
                         nn.BatchNorm2d(16),
                         nn.ReLU()
 
         )
         self.layer14 = nn.Sequential(
-                        nn.Conv2d(16, self.nc, 3, padding=0),
-                        nn.BatchNorm2d(self.nc),
-                        nn.ReLU()
+                        nn.Conv2d(16, self.nc, 3, padding=1)
 
         ) 
 
@@ -247,17 +244,18 @@ class SweatyNet2(nn.Module):
         out6 = self.pool(out5)
         out7 = self.layer8(self.layer7(self.layer6(out6)))
         
-        out7 = interpolate(out7, scale_factor=2, mode='bilinear', align_corners=True)
+        out7 = upsample(out7, scale_factor=2, mode='bilinear', align_corners=True)
         out7 = torch.cat((out5, out7), 1)
 
         out8 = self.layer11(self.layer10(self.layer9(out7)))
-        out8 = interpolate(out8, scale_factor=2, mode='bilinear', align_corners=True)
+        out8 = upsample(out8, scale_factor=2, mode='bilinear', align_corners=True)
         #out3 = F.pad(out3, pad=(2,2,2,2), mode='constant', value=0)
         out8 = torch.cat((out8, out3), 1)
 
         out = self.layer14(self.layer13(self.layer12(out8)))
+        out = F.softmax(out.squeeze().view(out.shape[0], -1)).view(out.shape[0], out.shape[2], out.shape[3])
         
-        return out.squeeze()
+        return out
 
 class SweatyNet3(nn.Module):
     def __init__(self, nc):
@@ -265,7 +263,7 @@ class SweatyNet3(nn.Module):
         self.nc = nc
         
         self.layer1 = nn.Sequential(
-                        nn.Conv2d(3, 8, 3, padding=0),
+                        nn.Conv2d(3, 8, 3, padding=1),
                         nn.BatchNorm2d(8),
                         nn.ReLU()
         )
@@ -346,11 +344,9 @@ class SweatyNet3(nn.Module):
                         nn.BatchNorm2d(64),
                         nn.ReLU()
 
-        )
-        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear')
-        
+        )        
         self.layer15 = nn.Sequential(
-                        nn.Conv2d(8+16+32+64+64, 64, 1, padding=1),
+                        nn.Conv2d(8+16+32+64+64, 64, 1, padding=0),
                         nn.BatchNorm2d(64),
                         nn.ReLU()
 
@@ -374,17 +370,16 @@ class SweatyNet3(nn.Module):
 
         )       
         self.layer19 = nn.Sequential(
-                        nn.Conv2d(16, 16, 3, padding=0),
+                        nn.Conv2d(16, 16, 3, padding=1),
                         nn.BatchNorm2d(16),
                         nn.ReLU()
 
         )
         self.layer20 = nn.Sequential(
-                        nn.Conv2d(16, self.nc, 3, padding=0),
-                        nn.BatchNorm2d(self.nc),
-                        nn.ReLU()
+                        nn.Conv2d(16, self.nc, 3, padding=1)
 
         ) 
+
     def forward(self, x):
         out1 = self.pool(self.layer1(x))
         out2 = torch.cat((out1, self.layer3(self.layer2(out1))), 1)
@@ -397,16 +392,19 @@ class SweatyNet3(nn.Module):
 
         out6 = self.pool(out5)
         out7 = self.layer14(self.layer13(self.layer12(self.layer11(self.layer10(out6)))))
-        out7 = torch.cat((out5, self.upsample(out7)), 1)
+        
+        out7 = upsample(out7, scale_factor=2, mode='bilinear', align_corners=True)
+        out7 = torch.cat((out5, out7), 1)
 
-        out8 = self.upsample(self.layer17(self.layer16(self.layer15(out7))))
+        out8 = self.layer17(self.layer16(self.layer15(out7)))
 
-        out3 = F.pad(out3, pad=(2,2,2,2), mode='constant', value=0)
+        out8 = upsample(out8, scale_factor=2, mode='bilinear', align_corners=True)
         out8 = torch.cat((out8, out3), 1)
 
         out = self.layer20(self.layer19(self.layer18(out8)))
-        
-        return out.squeeze()
+
+        out = F.softmax(out.squeeze().view(out.shape[0], -1)).view(out.shape[0], out.shape[2], out.shape[3])        
+        return out
 
 
 class ConvLSTM(nn.Module):
@@ -427,4 +425,5 @@ class ConvLSTM(nn.Module):
     	x = x.view(nc, batch_size, self.map_size[2]*self.map_size[3])
     	out, _ = self.lstm(x)
     	out = out[-1].view(batch_size, self.map_size[2], self.map_size[3])
-    	return out
+    	
+        return out
